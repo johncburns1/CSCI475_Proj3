efae05cb3cd93ffbd6fb8fd594b8f85eaad04ce4
xinu <xinu@osboxes>
2017-04-10 19:00:08 -0700

    Adjust pickup action

--- system/main.c
+++ system/main.c
@@ -5,11 +5,9 @@
 #include <stdlib.h>
 
 #define N 5	//number of philosophers and forks
-#define RAND_MAXIMUM	100
 
-//TODO - locks must be declared and initialized here
-mutex_t locks[N];	//default init for an array of ints is 0
-mutex_t print_lock = FALSE;
+mutex_t forks[N];		//default init for an array of ints is 0
+mutex_t mutex = FALSE;		//locking the print statements
 
 /**
  * Delay for a random amount of time
@@ -48,21 +46,23 @@ void	philosopher(uint32 phil_id)
 {
 
 	//TODO - right fork	
-	uint32 right = phil_id;
+	uint32 left = phil_id;
 	
 	//TODO - left fork
-	uint32 left;
+	uint32 right;
+
+	double odds;
 	
 	//for 1st philosopher
 	if(phil_id == 0)
 	{
-		left = N-1;
+		right = N-1;
 	}
 	
 	//every other philosopher
 	else	
 	{
-		left = phil_id - 1;
+		right = phil_id - 1;
 	}
 	
 	while (TRUE)
@@ -70,42 +70,72 @@ void	philosopher(uint32 phil_id)
 		//TODO
 		//think 70% of the time
 		//eat 30% of the time
+		int i;
 
-		double odds = (double)rand() / (double)((unsigned)RAND_MAX);
+		//gets a random number between [0,1]
+		for(i = 0; i < 10; i++)
+		{
+			odds = (double)rand() / (double)((unsigned)RAND_MAX);
+		}
 
 		//thinks
 		if(.3 < odds && odds <= 1)
 		{
-			mutex_lock(&print_lock);
+			//locks to print and then thinks
+			mutex_lock(&mutex);
 			kprintf("Philosopher %d thinking: zzzzzZZZz\n", phil_id);
-			mutex_unlock(&print_lock);
+			mutex_unlock(&mutex);
 			think();
 		}
 		
 		//eats
-		else if(0 <= odds && odds < .3)
+		else
 		{
+			//grab the right fork
+			mutex_lock(&forks[right]);
+
+			//check to see if the left fork is available
+			if(forks[left] == FALSE)
+			{
+				//lock the left fork and mutex to print then unlock them
+				mutex_lock(&forks[left]);
+				mutex_lock(&mutex);
+				kprintf("Philosopher %d eating: nom nom nom\n", phil_id);
+				mutex_unlock(&mutex);
+				eat();	
+				mutex_unlock(&forks[left]);
+				mutex_unlock(&forks[right]);
+			}
+			
+			//unlock
+			else
+			{
+				mutex_unlock(&forks[right]);
+			}
 			
 			//both are open
-			if(locks[right] == FALSE && locks[left] == FALSE)
+			/*
+			if(forks[right] == FALSE && forks[left] == FALSE)
 			{
-				mutex_lock(&print_lock);
-				mutex_unlock(&print_lock);
-				mutex_lock(&locks[right]);
-				mutex_lock(&locks[left]);
-				mutex_lock(&print_lock);
+				mutex_lock(&forks[right]);
+				mutex_lock(&forks[left]);
+				mutex_lock(&mutex);
 				kprintf("Philosopher %d eating: nom nom nom\n", phil_id);
-				mutex_unlock(&print_lock);
+				mutex_unlock(&mutex);
 				eat();	
-				mutex_unlock(&locks[right]);
-				mutex_unlock(&locks[left]);
-			}			
+				mutex_unlock(&forks[right]);
+				mutex_unlock(&forks[left]);
+			}
+			*/			
 		} 		
 	}
 }
 
 int	main(uint32 argc, uint32 *argv)
-{
+{	
+	//seed rand() from time of boot
+	srand(clktime);
+	
 	//do not change
 	ready(create((void*) philosopher, INITSTK, 15, "Ph1", 1, 0), FALSE);
 	ready(create((void*) philosopher, INITSTK, 15, "Ph2", 1, 1), FALSE);
